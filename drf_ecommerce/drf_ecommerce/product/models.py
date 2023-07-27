3c666f49fbb4ef2ca459f58a242a3913cb78838c
Ojas Acharekar <ojas.acharekar@godigitaltc.com>
2023-07-27 18:36:22 +0530

    minor change in model

--- drf_ecommerce/drf_ecommerce/product/models.py
+++ drf_ecommerce/drf_ecommerce/product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from mptt.models import MPTTModel,TreeForeignKey
 
 class Category(MPTTModel):
-    name = models.CharField(max_length=99,unique=True)
+    name = models.CharField(max_length=199,unique=True)
     parent = TreeForeignKey("self",on_delete=models.PROTECT,null=True,blank=True)
 
     class MPTTMeta:
